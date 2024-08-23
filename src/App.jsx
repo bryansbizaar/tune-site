d888bb3544f93591e06a65c2930c2f82f24e8007
Bryan <bryan.owens@developersinstitute.co.nz>
2024-08-23 22:26:14 +1200

    Updated router in app.jsx file to include endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import "./index.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/tune-site">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
