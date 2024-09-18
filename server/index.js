6917f2c3361c86c4f6512127599d687f84977872
Bryan <bryan.owens@developersinstitute.co.nz>
2024-09-18 12:11:53 +1200

    Removed excess code from index.js file

--- server/index.js
+++ server/index.js
@@ -1,22 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/api/tunes", (req, res) => {
-//   // This is where you'll eventually fetch tunes from a database
-//   res.json([{ id: 1, title: "Sample Tune", hasExternalLink: true }]);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
-
 const express = require("express");
 const path = require("path");
 const fs = require("fs").promises;
