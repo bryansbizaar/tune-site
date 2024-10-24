e06261a98de5e645e68d1d9b8179ddf54b5704c7
Bryan <bryan.owens@developersinstitute.co.nz>
2024-10-24 22:00:19 +1300

    Updated package.json, added initial ci/cd pipeline

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/tune-site/",
   publicDir: "public",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
