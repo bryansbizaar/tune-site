0678a1751665d23e750e6afbb405f0c3680edc33
Bryan <bryan.owens@developersinstitute.co.nz>
2024-11-06 11:14:28 +1300

    Removed unused and commented-out code from server files

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  // try {
-  //   await mongoose.connect(process.env.MONGODB_URI);
-  //   console.log("Connected to MongoDB successfully");
-  // } catch (error) {
-  //   console.error("MongoDB connection error:", error);
-  //   process.exit(1);
-  // }
   mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
